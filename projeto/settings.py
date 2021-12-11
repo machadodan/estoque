1b892fe65a628b41c3fa61d87725f7adec0bc843
machadodan <danielg_m@hotmail.com>
2021-12-11 15:16:11 -0300

    criar app produto. Close#5

--- projeto/settings.py
+++ projeto/settings.py
@@ -39,8 +39,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
     #myapps
     'projeto.core',
+    'projeto.produto',
 
 ]
 
