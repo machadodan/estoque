50e9550e10654cb0fa6f405dcc1b37ad1063aeca
machadodan <danielg_m@hotmail.com>
2022-01-12 18:55:41 -0300

    criar editar produto. close#14

--- projeto/produto/urls.py
+++ projeto/produto/urls.py
@@ -8,6 +8,7 @@ urlpatterns = [
     path('', v.produto_list, name='produto_list'),
     path('<int:pk>/', v.produto_detail, name='produto_detail'),
     path('add', v.ProdutoCreate.as_view(), name='produto_add'),
+    path('<int:pk>/edit/', v.ProdutoUpdate.as_view(), name='produto_edit'),
 
 
 ]
