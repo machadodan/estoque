67351cef50c67cd0402167d7e62ee65391a48415
machadodan <danielg_m@hotmail.com>
2022-01-21 20:19:36 -0300

    Estoque #33 criando apenas 1 estoque detail.Close

--- projeto/estoque/models.py
+++ projeto/estoque/models.py
@@ -40,10 +40,6 @@ class EstoqueEntrada(Estoque):
         verbose_name = 'estoque entrada'
         verbose_name_plural = 'estoque entrada'
 
-    def get_absolute_url(self):
-        return reverse_lazy('estoque:estoque_entrada_detail', kwargs={'pk': self.pk})
-
-
 class EstoqueSaida(Estoque):
 
     objects = EstoqueSaidaManager()
@@ -52,10 +48,6 @@ class EstoqueSaida(Estoque):
         proxy = True
         verbose_name = 'estoque saida'
         verbose_name_plural = 'estoque saida'
-
-    def get_absolute_url(self):
-        return reverse_lazy('estoque:estoque_saida_detail', kwargs={'pk':self.pk})
-
 
 class EstoqueItens(models.Model):
     estoque = models.ForeignKey(Estoque, on_delete=models.CASCADE, related_name='estoques')
