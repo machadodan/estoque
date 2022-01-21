0ef01e1057658a8424668eb0009e79e3ab5d22e4
machadodan <danielg_m@hotmail.com>
2022-01-21 19:48:06 -0300

    Estoque #32 Transformando estoque_entrada_detail em CBV.Close

--- projeto/estoque/views.py
+++ projeto/estoque/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, resolve_url
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
 from django.forms import inlineformset_factory
 from projeto.produto.models import Produto
 from django.http import HttpResponseRedirect
@@ -35,6 +35,16 @@ def estoque_entrada_detail(request, pk):
         'url_list': 'estoque:estoque_entrada_list'
     }
     return render(request, template_name, context)
+
+
+class EstoqueEntradaDetail(DetailView):
+    model = EstoqueEntrada
+    template_name = 'estoque_detail.html'
+
+    def get_context_data(self, **kwargs):
+        context= super(EstoqueEntradaDetail, self).get_context_data(**kwargs)
+        context['url_list'] ='estoque:estoque_entrada_list'
+        return context
 
 
 def dar_baixa_estoque(form):
@@ -117,6 +127,16 @@ def estoque_saida_detail(request, pk):
         'url_list': 'estoque:estoque_saida_list'
                }
     return render(request, template_name, context)
+
+
+class EstoqueSaidaDetail(DetailView):
+    model = EstoqueSaida
+    template_name = 'estoque_detail.html'
+
+    def get_context_data(self, **kwargs):
+        context= super(EstoqueSaidaDetail, self).get_context_data(**kwargs)
+        context['url_list'] ='estoque:estoque_saida_list'
+        return context
 
 
 def estoque_saida_add(request):
