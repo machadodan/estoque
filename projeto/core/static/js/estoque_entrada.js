6b44edeededb4a7cac3141b071e2107527a10b40
machadodan <danielg_m@hotmail.com>
2022-01-25 20:09:56 -0300

    Desabilitar campo 'Saldo' na entrada e saida.Close #39

--- projeto/core/static/js/estoque_entrada.js
+++ projeto/core/static/js/estoque_entrada.js
@@ -3,6 +3,11 @@
     //insere classe no primeiro item do produto
     $('#id_estoque-0-produto').addClass('clProduto');
     $('#id_estoque-0-quantidade').addClass('clQuantidade');
+    //Desabilita o primeiro campo saldo
+    $('#id_estoque-0-saldo').prop('type', 'hidden')
+    //Cria um spam para mostrar saldo na tela
+    $('label[for="id_estoque-0-saldo"]').append('<span id="id_estoque-0-saldo-span" class="lead" style="padding-left: 10px;"></span>')
+
 
      $('#add-item').click(function(ev){
        ev.preventDefault();
@@ -14,6 +19,9 @@
      //update form count
      $('#id_estoque-TOTAL_FORMS').attr('value', count + 1);
 
+     //Desabilita o campo saldo
+     $('#id_estoque-'+ (count) +'-saldo').prop('type', 'hidden')
+
       //some animate to scroll to view our new form
      $('html, body').animate({
         scrollTop: $("#add-item").position().top - 200
@@ -21,12 +29,16 @@
 
       $('#id_estoque-' + (count) + '-produto').addClass('clProduto');
       $('#id_estoque-' + (count) + '-quantidade').addClass('clQuantidade');
+
+      //Cria um spam para mostrar saldo na tela
+    $('label[for="id_estoque-' + (count) + '-saldo"]').append('<span id="id_estoque-' + (count) + '-saldo-span" class="lead" style="padding-left: 10px;"></span>')
       });
     });
 
     let estoque
     let saldo
     let campo
+    let campo2
     let quantidade
 
     $(document).on('change', '.clProduto', function() {
@@ -52,7 +64,11 @@
         quantidade = $(this).val();
         saldo = Number(quantidade) + Number(estoque);
         campo = $(this).attr('id').replace('quantidade', 'saldo')
-
+        //Desabilita o campo saldo
+        $('#'+campo).prop('type', 'hidden')
         //atribui o saldo ao campo saldo
         $('#'+campo).val(saldo)
+        campo2 = $(this).attr('id').replace('quantidade', 'saldo-span')
+        //Atribui o saldo ao campo 'id_estoque-x-saldo-span'
+        $('#'+campo2).text(saldo)
     });
